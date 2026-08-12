cf53308a927f0748626610ee93ee8febaf5a6a8c
Puppet Admin <your-email@example.com>
2026-08-12 19:21:35 +0530

    Move demo manifest to manifests directory

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,4 @@
-node 'puppetclient.example.com' {
-
-  $message = lookup('message')
-
-  file { '/tmp/r10k-hiera-test.txt':
-    ensure  => file,
-    content => "${message}\n",
-  }
-
+file { '/tmp/puppet-demo.txt':
+  ensure  => file,
+  content => "Hello from Puppet Server!\n",
 }
--- site/site.pp
+++
@@ -1,4 +0,0 @@
-file { '/tmp/puppet-demo.txt':
-  ensure  => file,
-  content => "Hello from Puppet Server!\n",
-}
