bd3db6a7323d2a55a5055b4a47dd0f6d49267410
Puppet Admin <your-email@example.com>
2026-08-18 18:43:54 +0530

    Update Hiera message to hello sabarirajan

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,39 @@
 node 'puppetclient.example.com' {
 
+
+  $message = lookup('message')
+
+
+  file { '/tmp/r10k-hiera-test.txt':
+    ensure  => file,
+    content => "${message}\n",
+  }
+
+
+  file { '/tmp/puppet-demo.txt':
+    ensure  => file,
+    content => "Hello from Puppet Server!\n",
+  }
+
+
+  file { '/tmp/hiera-test.txt':
+    ensure  => file,
+    content => lookup('message'),
+  }
+
+
   package { 'httpd':
     ensure => installed,
   }
+
 
   service { 'httpd':
     ensure  => running,
     enable  => true,
     require => Package['httpd'],
-
   }
+
+
   package { 'chrony':
     ensure => installed,
   }
